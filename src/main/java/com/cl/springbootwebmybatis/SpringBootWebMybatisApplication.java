b7ca41cd80930d1efb2d9b8e26b30c08b82e3429
cailong <cailong1028@126.com>
2018-06-21 11:39:36 +0800

    init

---
+++ src/main/java/com/cl/springbootwebmybatis/SpringBootWebMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.cl.springbootwebmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebMybatisApplication.class, args);
+	}
+}
