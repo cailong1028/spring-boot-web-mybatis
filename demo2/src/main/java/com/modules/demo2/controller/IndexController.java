5b9bd8bfeca8acb83dff1208524c8dbfcc5caca9
cailong <cailong1028@126.com>
2018-12-27 09:41:34 +0800

    配置静态目录，thymeleaf模版，以及路由

--- demo2/src/main/java/com/modules/demo2/controller/IndexController.java
+++ demo2/src/main/java/com/modules/demo2/controller/IndexController.java
@@ -1,15 +1,22 @@
 package com.modules.demo2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 public class IndexController {
 
-    @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String test(){
-        return "i am index path2";
+    @RequestMapping(value = "/")
+    public String index(HttpServletRequest request, HttpServletResponse response){
+        return "template";
     }
 
+    @RequestMapping(value = "/temp2")
+    public String index2(HttpServletRequest request, HttpServletResponse response){
+        return "template2";
+    }
 }
